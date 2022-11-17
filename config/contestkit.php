63d81f8de946b454b070d83fe39bf87bb628c8f5
Ninja <yazjallad@gmail.com>
2022-11-17 15:30:58 -0800

    Update contestkit.php

--- config/contestkit.php
+++ config/contestkit.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'base_url' => env('CONTESTKIT_HOST', 'https://contestkit.app/api/v1'),
+    'base_url' => env('CONTESTKIT_APi_URL', 'https://contestkit.app/api/v1'),
     'headers' => [
         'Content-Type' => 'application/json',
         'Accept' => 'application/json',
     ],
 
-    'access_token' =>  env('CONTESTKIT_TOKEN'),
+    'access_token' =>  env('CONTESTKIT_API_TOKEN'),
 ];
