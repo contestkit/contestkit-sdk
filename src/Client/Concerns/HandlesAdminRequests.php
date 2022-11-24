2a7dbbaf12870d4971856bba9d12188a3c82dfdf
Ninja <yazjallad@gmail.com>
2022-11-24 15:55:03 -0800

    add space

--- src/Client/Concerns/HandlesAdminRequests.php
+++ src/Client/Concerns/HandlesAdminRequests.php
@@ -23,6 +23,7 @@ trait HandlesAdminRequests
 
         return $request->json('data');
     }
+    
     public function accountPromotionCampaigns(int $promotionId)
     {
         $request = $this->getClient()
