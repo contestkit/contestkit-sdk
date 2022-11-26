611079a974884a54b9b3dea672d19995bb9c4d63
ninjaparade <ninjaparade@users.noreply.github.com>
2022-11-26 03:23:35 +0000

    Fix styling

--- src/Client/Concerns/InteractsWithPromotions.php
+++ src/Client/Concerns/InteractsWithPromotions.php
@@ -1,33 +1,35 @@
 <?php
 
 namespace ContestKit\Sdk\Client\Concerns;
+
 use Illuminate\Validation\ValidationException;
+
 trait InteractsWithPromotions
 {
-   public function promotion(string $promotion, array $with = [])
+    public function promotion(string $promotion, array $with = [])
     {
         $request = $this->getClient()
-            ->get("{$promotion}/promotion", ['with'=> $with]);
+            ->get("{$promotion}/promotion", ['with' => $with]);
 
         $this->handleRequest(request: $request);
 
         return $request->json('data');
     }
 
-    public function earlyAccess(string $promotion, array $data = [])
-    {
-        $request = $this->getClient()
-            ->post("{$promotion}/early-access", $data);
+     public function earlyAccess(string $promotion, array $data = [])
+     {
+         $request = $this->getClient()
+             ->post("{$promotion}/early-access", $data);
 
-        if ($request->clientError() && $request->status() === 422) {
-            throw ValidationException::withMessages($request->json()['errors']);
-        }
-        if ($request->clientError() ) {
-            throw ValidationException::withMessages($request->json()['errors']);
-        }
+         if ($request->clientError() && $request->status() === 422) {
+             throw ValidationException::withMessages($request->json()['errors']);
+         }
+         if ($request->clientError()) {
+             throw ValidationException::withMessages($request->json()['errors']);
+         }
 
-        $this->handleRequest(request: $request);
+         $this->handleRequest(request: $request);
 
-        return $request->json();
-    }
+         return $request->json();
+     }
 }
