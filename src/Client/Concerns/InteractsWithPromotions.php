7ef43ddfcaf1fd28ec649318717f7d3f39e37135
Ninja <yazjallad@gmail.com>
2022-11-25 19:23:17 -0800

    fix

--- src/Client/Concerns/InteractsWithPromotions.php
+++ src/Client/Concerns/InteractsWithPromotions.php
@@ -1,16 +1,33 @@
 <?php
 
 namespace ContestKit\Sdk\Client\Concerns;
-
+use Illuminate\Validation\ValidationException;
 trait InteractsWithPromotions
 {
-    public function promotion(string $promotion)
+   public function promotion(string $promotion, array $with = [])
     {
         $request = $this->getClient()
-            ->get("{$promotion}/promotion");
-        dd($request->json());
+            ->get("{$promotion}/promotion", ['with'=> $with]);
+
         $this->handleRequest(request: $request);
 
         return $request->json('data');
+    }
+
+    public function earlyAccess(string $promotion, array $data = [])
+    {
+        $request = $this->getClient()
+            ->post("{$promotion}/early-access", $data);
+
+        if ($request->clientError() && $request->status() === 422) {
+            throw ValidationException::withMessages($request->json()['errors']);
+        }
+        if ($request->clientError() ) {
+            throw ValidationException::withMessages($request->json()['errors']);
+        }
+
+        $this->handleRequest(request: $request);
+
+        return $request->json();
     }
 }
