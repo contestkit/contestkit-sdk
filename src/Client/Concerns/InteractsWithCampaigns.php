bd5f89a2e8e8fa49e1ade6004d6fa2b677d27504
Ninja <yazjallad@gmail.com>
2022-12-01 10:36:35 -0800

    Update SDK

--- src/Client/Concerns/InteractsWithCampaigns.php
+++ src/Client/Concerns/InteractsWithCampaigns.php
@@ -13,4 +13,14 @@ trait InteractsWithCampaigns
 
         return $request->json('data');
     }
+
+    public function campaignPrizes(string $campaign, bool $featured = false)
+    {
+        $request = $this->getClient()
+            ->get("{$campaign}/campaign/prizes", ['featured' => $featured ]);
+
+        $this->handleRequest(request: $request);
+
+        return $request->json('data');
+    }
 }
--- src/Client/Concerns/InteractsWithPromotions.php
+++ src/Client/Concerns/InteractsWithPromotions.php
@@ -16,6 +16,16 @@ trait InteractsWithPromotions
         return $request->json('data');
     }
 
+    public function promotionPrizes(string $promotion, bool $featured = false)
+    {
+        $request = $this->getClient()
+            ->get("{$promotion}/promotion/prizes", ['featured' => $featured]);
+
+        $this->handleRequest(request: $request);
+
+        return $request->json('data');
+    }
+
      public function earlyAccess(string $promotion, array $data = [])
      {
          $request = $this->getClient()
